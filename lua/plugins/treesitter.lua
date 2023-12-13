a172cf501f4764dd1cb2555f126ecb977f2524ea
mihavintar <mhckhc.vintar@gmail.com>
2023-12-13 10:35:26 +0100

    Added extras
    Removed tagbar and replaced it with symbol outlines and added navic.

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "python",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+        "cpp",
+      },
+    },
+  },
+}
