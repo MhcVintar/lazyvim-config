bf2c6833372c4bac29ad2a952fc1c301b63a107a
mihav <mhckhc.vintar@gmail.com>
2024-02-06 13:00:40 +0100

    added neocolumn back

---
+++ lua/plugins/neocolumn.lua
@@ -0,0 +1,16 @@
+return {
+  "ecthelionvi/NeoColumn.nvim",
+  opts = {
+    always_on = true,
+  },
+  cmd = {
+    "ToggleNeoCollumn",
+  },
+  keys = {
+    {
+      "<leader>cc",
+      "<cmd>ToggleNeoColumn<cr>",
+      desc = "Toggle ColorColumn",
+    },
+  },
+}
