1fc39e49a5ab581962f9c23c8e8dfc09a65b0009
mihav <mhckhc.vintar@gmail.com>
2024-03-24 15:59:35 +0100

    improved the icon picker

--- lua/plugins/icon-picker.lua
+++ lua/plugins/icon-picker.lua
@@ -1,27 +1,63 @@
 return {
-  "ziontee113/icon-picker.nvim",
-  dependencies = {
-    {
-      "stevearc/dressing.nvim",
+  {
+    "ziontee113/icon-picker.nvim",
+    dependencies = {
+      {
+        "stevearc/dressing.nvim",
+      },
+      {
+        "nvim-telescope/telescope.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+      },
     },
-    {
-      "nvim-telescope/telescope.nvim",
-      dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      {
+        "<leader>ia",
+        "<cmd>IconPickerNormal<cr>",
+        desc = "All Icons",
+      },
+      {
+        "<leader>if",
+        "<cmd>IconPickerNormal alt_font<cr>",
+        desc = "Alternative Fonts",
+      },
+      {
+        "<leader>ie",
+        "<cmd>IconPickerNormal emoji<cr>",
+        desc = "Emojis",
+      },
+      {
+        "<leader>ih",
+        "<cmd>IconPickerNormal html_colors<cr>",
+        desc = "HTML Colors",
+      },
+      {
+        "<leader>in",
+        "<cmd>IconPickerNormal nerd_font<cr>",
+        desc = "Nerd Fonts",
+      },
+      {
+        "<leader>is",
+        "<cmd>IconPickerNormal symbols<cr>",
+        desc = "Symbols",
+      },
+    },
+    cmd = {
+      "IconPickerNormal",
+      "IconPickerYank",
+      "IconPickerInsert",
+    },
+    config = function()
+      require("icon-picker").setup({ disable_legacy_commands = true })
+    end,
+  },
+  {
+    "folke/which-key.nvim",
+    optional = true,
+    opts = {
+      defaults = {
+        ["<leader>i"] = { name = "+icons" },
+      },
     },
   },
-  keys = {
-    {
-      "<leader>i",
-      "<cmd>IconPickerNormal<cr>",
-      desc = "Icon picker",
-    },
-  },
-  cmd = {
-    "IconPickerNormal",
-    "IconPickerYank",
-    "IconPickerInsert",
-  },
-  config = function()
-    require("icon-picker").setup({ disable_legacy_commands = true })
-  end,
 }
